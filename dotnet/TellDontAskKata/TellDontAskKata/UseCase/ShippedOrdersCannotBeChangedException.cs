8ec8e4b58d00c8421f2304563bc325eea98a54d9
Vincent Farah <vincent.farah@codurance.com>
2018-08-21 13:07:25 +0100

    Added ShippedOrdersCannotBeChangedException

---
+++ dotnet/TellDontAskKata/TellDontAskKata/UseCase/ShippedOrdersCannotBeChangedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TellDontAskKata.UseCase
+{
+    public class ShippedOrdersCannotBeChangedException : Exception
+    {
+        public ShippedOrdersCannotBeChangedException()
+        {
+        }
+
+        public ShippedOrdersCannotBeChangedException(string message) : base(message)
+        {
+        }
+
+        public ShippedOrdersCannotBeChangedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ShippedOrdersCannotBeChangedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
