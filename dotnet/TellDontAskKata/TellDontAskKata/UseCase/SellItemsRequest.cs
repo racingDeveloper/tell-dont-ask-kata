3c2ff733660405406f4193aaf5aa6601ac833869
Vincent Farah <vincent.farah@codurance.com>
2018-08-21 13:10:27 +0100

    Finished SellItemsRequest

---
+++ dotnet/TellDontAskKata/TellDontAskKata/UseCase/SellItemsRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TellDontAskKata.UseCase
+{
+    public class SellItemsRequest
+    {
+        public List<SellItemsRequest> Requests { get; set; }
+    }
+}
