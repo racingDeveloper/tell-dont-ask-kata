51a93789852b14879089580720305923a985ee20
christopher-bimson <christopher-bimson>
2018-08-21 12:53:31 +0100

    Add IOrderRepository

---
+++ dotnet/TellDontAskKata/TellDontAskKata/Repository/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using TellDontAskKata.Domain;
+
+namespace TellDontAskKata.Repository
+{
+    public interface IOrderRepository
+    {
+        void Save(Order order);
+        Order GetById(int orderId);
+    }
+}
