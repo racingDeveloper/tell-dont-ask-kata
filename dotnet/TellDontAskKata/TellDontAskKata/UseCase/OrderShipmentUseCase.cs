36476c2505c69fcb19c15a8b6f9d9d263688491f
Vincent Farah <vincent.farah@codurance.com>
2018-08-21 13:34:58 +0100

    Started OrderShipmentUseCase

---
+++ dotnet/TellDontAskKata/TellDontAskKata/UseCase/OrderShipmentUseCase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TellDontAskKata.Domain;
+using TellDontAskKata.Repository;
+using TellDontAskKata.Service;
+
+namespace TellDontAskKata.UseCase
+{
+    public class OrderShipmentUseCase
+    {
+        private readonly IOrderRepository orderRepository;
+
+        private readonly IShipmentService shipmentService;
+
+        public OrderShipmentUseCase(IOrderRepository orderRepository, IShipmentService shipmentService)
+        {
+            this.orderRepository = orderRepository;
+            this.shipmentService = shipmentService;
+        }
+
+        public void Run(OrderShipmentRequest request)
+        {
+            //Order order = this.orderRepository.GetById(request.OrderId);
+
+            //if (order.Status = OrderStatus.Created || order.Status == OrderStatus.Rejected)
+            //{
+            //    throw new OrderCannotBeShippedException();
+            //}
+            //
+            // if (order.getStatus().equals(SHIPPED))
+            // {
+            //     throw new OrderCannotBeShippedTwiceException();
+            // }
+            //
+            // shipmentService.ship(order);
+
+            // order.setStatus(OrderStatus.SHIPPED);
+            // orderRepository.save(order);
+        }
+    }
+}
