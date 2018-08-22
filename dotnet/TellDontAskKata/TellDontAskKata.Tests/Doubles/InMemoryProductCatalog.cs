adc36e2ee2b21263173d6f4166c43e3702a99bc0
Vincent Farah <vincent.farah@codurance.com>
2018-08-22 15:00:23 +0100

    Added IInMemoryProductCatalog

---
+++ dotnet/TellDontAskKata/TellDontAskKata.Tests/Doubles/InMemoryProductCatalog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TellDontAskKata.Tests.Doubles
+{
+    using System.Linq;
+
+    using TellDontAskKata.Domain;
+    using TellDontAskKata.Repository;
+    public class InMemoryProductCatalog : IProductCatalog
+    {
+        private readonly IEnumerable<Product> products;
+
+        public InMemoryProductCatalog(IEnumerable<Product> products)
+        {
+            this.products = products;
+        }
+
+        public Product GetByName(string name)
+        {
+            return this.products.SingleOrDefault(item => item.Name.Equals(name));
+        }
+    }
+}
