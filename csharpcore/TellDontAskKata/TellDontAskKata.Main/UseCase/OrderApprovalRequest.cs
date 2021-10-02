39c407c0989152c4ad0453f934e5117789ab1376
Matthew Brown <matthew.brown@soton.ac.uk>
2021-10-02 13:39:50 +0100

    Fix an incorrect filename

--- csharpcore/TellDontAskKata/TellDontAskKata.Main/UseCase/OrderApprovalRequest.cs
+++ csharpcore/TellDontAskKata/TellDontAskKata.Main/UseCase/OrderApprovalRequest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TellDontAskKata.Main.UseCase
+﻿namespace TellDontAskKata.Main.UseCase
 {
     public class OrderApprovalRequest
     {
