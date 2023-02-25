810cfac805f1ee86e4d03bc1aef6eb2e397abf06
humaimam <huma.imam@bazaar-app.com>
2023-02-25 16:23:40 +0500

    Add optionals to handle null

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
@@ -9,6 +9,7 @@ import it.gabrieletondi.telldontaskkata.repository.ProductCatalog;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static java.math.BigDecimal.valueOf;
 import static java.math.RoundingMode.HALF_UP;
@@ -24,33 +25,35 @@ public class OrderCreationUseCase {
 
     public void run(SellItemsRequest request) {
 
-        Order order = new Order(new BigDecimal("0.00"),"EUR",new ArrayList<>(),new BigDecimal("0.00"),OrderStatus.CREATED);
+        Order order = new Order(new BigDecimal("0.00"), "EUR", new ArrayList<>(), new BigDecimal("0.00"), OrderStatus.CREATED);
 
         for (SellItemRequest itemRequest : request.getRequests()) {
             Product product = productCatalog.getByName(itemRequest.getProductName());
 
-            if (product == null) {
-                throw new UnknownProductException();
-            } else {
-                BigDecimal productPrice = product.getPrice();
-                final BigDecimal unitaryTax = product.getUnitaryTax(productPrice);
+            Optional.ofNullable(product).orElseThrow(() -> new UnknownProductException());
 
-                final BigDecimal unitaryTaxedAmount = productPrice.add(unitaryTax).setScale(2, HALF_UP);
-                final BigDecimal taxedAmount = getTaxedAmount(unitaryTaxedAmount, BigDecimal.valueOf(itemRequest.getQuantity()))
-                        .setScale(2, HALF_UP);
-                final BigDecimal taxAmount = getTaxedAmount(unitaryTax, BigDecimal.valueOf(itemRequest.getQuantity()));
+            BigDecimal productPrice = product.getPrice();
+            final BigDecimal unitaryTax = product.getUnitaryTax(productPrice);
 
-                final OrderItem orderItem = new OrderItem(product, itemRequest.getQuantity(), taxedAmount, taxAmount);
-                order.addOrderItem(orderItem);
+            final BigDecimal unitaryTaxedAmount = productPrice.add(unitaryTax).setScale(2, HALF_UP);
+            final BigDecimal taxedAmount = getTaxedAmount(unitaryTaxedAmount, BigDecimal.valueOf(itemRequest.getQuantity()))
+                    .setScale(2, HALF_UP);
+            final BigDecimal taxAmount = getTaxedAmount(unitaryTax, BigDecimal.valueOf(itemRequest.getQuantity()));
 
-                order.addTotal(taxedAmount);
-                order.addTax(taxAmount);
-            }
+            final OrderItem orderItem = new OrderItem(product, itemRequest.getQuantity(), taxedAmount, taxAmount);
+            order.addOrderItem(orderItem);
+
+            order.addTotal(taxedAmount);
+            order.addTax(taxAmount);
+
         }
 
         orderRepository.save(order);
     }
-    public BigDecimal getTaxedAmount(BigDecimal amount, BigDecimal quantity){
+
+    public BigDecimal getTaxedAmount(BigDecimal amount, BigDecimal quantity) {
         return amount.multiply(quantity);
     }
+
+
 }
